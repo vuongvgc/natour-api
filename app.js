617b57f0c94b5373579d2faebb9de3e4e717e09a
vuongvgc <vuongvgc@gmail.com>
2021-06-13 20:03:45 +0700

    our API hanlding get Request

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-  //   res.status(200).send('Hello from the server side');
-  res
-    .status(200)
-    .json({ messenger: 'Hello from the server side', app: 'natour' });
+// app.get('/', (req, res) => {
+//   //   res.status(200).send('Hello from the server side');
+//   res
+//     .status(200)
+//     .json({ messenger: 'Hello from the server side', app: 'natour' });
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
+
 app.post('/', (req, res) => {
   res.status(404).send('Page not found');
 });
