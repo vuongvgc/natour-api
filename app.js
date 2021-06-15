2c3202c114f50690724a44c654c5c8ad1b273dd9
vuongvgc <vuongvgc@gmail.com>
2021-06-15 22:04:30 +0700

    refactoring routes: create server file split express file and server

--- app.js
+++ app.js
@@ -34,8 +34,4 @@ app.use(morgan('dev'));
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
 
-// 3) START SERVER
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
