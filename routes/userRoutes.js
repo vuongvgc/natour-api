68067d90d867c0fcd9f69f7883295d5e9b432efd
vuongvgc <vuongvgc@gmail.com>
2021-06-15 22:02:12 +0700

    refactoring routes: create tour and user controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,40 +1,13 @@
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require('./../controllers/userController');
 const express = require('express');
-// ) ROUTER HANDLE
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'The route is not defined',
-  });
-};
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'The route is not defined',
-  });
-};
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'The route is not defined',
-  });
-};
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'The route is not defined',
-  });
-};
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'The route is not defined',
-  });
-};
-
 // ROUTES
 const router = express.Router();
-
 // USER
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
