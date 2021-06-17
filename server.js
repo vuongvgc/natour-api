bafc19001c74a0a225681a34f66b2b17f64a6af1
vuongvgc <vuongvgc@gmail.com>
2021-06-17 22:43:26 +0700

    eslint config prettier

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+const a = 2;
+a = 3;
 // console.log(app.get('env'));
 console.log(process.env);
 const port = process.env.PORT || 3000;
