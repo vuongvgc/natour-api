38f9d365768613b5f0e877bc6e04269d413631da
vuongvgc <vuongvgc@gmail.com>
2021-06-21 21:33:36 +0700

    Create Schema and model-  Create document and test

---
+++ models/toursModel.js
@@ -0,0 +1,16 @@
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 5.3,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
--- server.js
+++ server.js
@@ -10,19 +10,52 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
-    // .connect(DB, {
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then((con) => {
-    console.log(con.connection);
+  .then(() => {
+    // console.log(con.connection);
     console.log('DB is Connection');
   });
 
 // console.log(app.get('env'));
 // console.log(process.env);
+// const tourSchema = new mongoose.Schema({
+//   name: String,
+//   price: Number,
+//   rating: Number,
+// });
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 5.3,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'The Forest Hiker Vietnam',
+  price: 293,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Create Error', err);
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   // console.log(`App running on port ${port}`);
