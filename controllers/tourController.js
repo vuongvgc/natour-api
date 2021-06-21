4a9af8fa8fe1d9023671e3ebdbdbd26732e2e71e
vuongvgc <vuongvgc@gmail.com>
2021-06-21 22:33:21 +0700

    create document with real data use create: async await

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,7 +12,7 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
   // const newTour = new Tour({
   //   ...req.body,
   // });
@@ -27,30 +27,32 @@ exports.createTour = (req, res) => {
   //   .catch((err) => {
   //
   //   });
-  // try {
-  //   const newTour = await Tour.create(req.body);
-  //   res.status(200).json({
-  //     status: 'Success',
-  //     tour: newTour,
-  //   });
-  // } catch (err) {
-  //   res.status(400).json({
-  //     status: 'Bad Request',
-  //     message: err,
-  //   });
-  // }
-  Tour.create(req.body, (doc, err) => {
-    if (err) {
-      res.status(400).json({
-        status: 'Bad Request',
-        message: err,
-      });
-    }
+  try {
+    const newTour = await Tour.create(req.body);
     res.status(200).json({
       status: 'Success',
-      tour: doc,
+      data: {
+        tour: newTour,
+      },
     });
-  });
+  } catch (err) {
+    res.status(400).json({
+      status: 'Bad Request',
+      message: err,
+    });
+  }
+  // Tour.create(req.body, (doc, err) => {
+  //   if (err) {
+  //     res.status(400).json({
+  //       status: 'Bad Request',
+  //       message: err,
+  //     });
+  //   }
+  //   res.status(200).json({
+  //     status: 'Success',
+  //     tour: doc,
+  //   });
+  // });
 };
 exports.updateTour = (req, res) => {
   res.status(200).json({
