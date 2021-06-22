f3ec81206fad63af576a90d8645f4ed7c1fcc658
vuongvgc <vuongvgc@gmail.com>
2021-06-22 21:01:35 +0700

    read  documment to db use find id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,6 +21,7 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
+    // const tour = await Tour.find({ _id: req.params.id });
     res.status(200).json({
       status: 'success',
       data: {
