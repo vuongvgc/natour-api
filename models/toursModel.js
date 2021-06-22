11173301458fe179fbff04fca69e4e4a535c8bfc
vuongvgc <vuongvgc@gmail.com>
2021-06-22 22:34:02 +0700

    Create Schema for tours

--- models/toursModel.js
+++ models/toursModel.js
@@ -5,15 +5,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
     type: Number,
-    default: 5.3,
+    required: [true, 'Tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a maxGroupSize'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour must have difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'Tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have a Summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Tour must have a image cover'],
+  },
+  images: [String],
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
