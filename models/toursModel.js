08b2ab15592a95ae0e43a4403671f811d0b7e379
vuongvgc <vuongvgc@gmail.com>
2021-06-27 22:22:20 +0700

    document middleware: save pre and post

--- models/toursModel.js
+++ models/toursModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'Tour must have a duration'],
@@ -62,5 +64,21 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeek').get(function () {
   return this.duration / 7;
 });
+// DOCUMENT MIDDLEWARE: run before.save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+// DOCUMENT MIDDLEWARE: WILL SAVE DOCUMENT
+tourSchema.pre('save', (next) => {
+  console.log('Will save Documnet');
+  next();
+});
+// DOCUMENT MIDDLEWARE: run after save or create done
+tourSchema.post('save', (doc, next) => {
+  console.log(doc);
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
