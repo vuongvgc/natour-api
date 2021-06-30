3657aae1d3535e23b2e2df9297dc36bb179cc938
vuongvgc <vuongvgc@gmail.com>
2021-06-30 22:02:57 +0700

    create handing error for router underfine

--- app.js
+++ app.js
@@ -37,4 +37,11 @@ app.use(express.static('public'));
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can not find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
