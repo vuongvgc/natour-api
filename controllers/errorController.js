b018adda726d34a229bed281e351c063c04b2ef4
vuongvgc <vuongvgc@gmail.com>
2021-07-04 22:44:55 +0700

    hanlding invalid databaseID

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (error) => {
+  const message = `Invalid ${error._id}: ${error.value}`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (res, err) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -8,6 +14,7 @@ const sendErrorDev = (res, err) => {
 };
 const sendErrorProd = (res, err) => {
   //Operational, trusted error: send message to client
+
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
@@ -26,10 +33,14 @@ const sendErrorProd = (res, err) => {
 };
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || 'error';
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(res, err);
   }
   if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(res, err);
   }
 };
