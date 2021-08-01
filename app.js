3c326d25e0c2c90c18dbe44be6a9d1bd85e10f68
vuongvgc <vuongvgc@gmail.com>
2021-08-01 21:06:51 +0700

    security: prevent parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalControlHandler = require('./controllers/errorController');
 const tourRoutes = require('./routes/tourRoutes');
@@ -25,11 +26,22 @@ app.use('/api', limitedRequestFromAnAPI);
 
 // Body parse ,reading data from body into res.body
 app.use(express.json({ limit: '10kb' }));
-
 // Data sanitize against NoSQL query injection
 app.use(mongoSanitize());
 // Data sanitize against XSS
 app.use(xss());
+// Prevent Parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 // Serve satic file
 app.use(express.static('public'));
 app.use('/api/v1/tours', tourRoutes);
